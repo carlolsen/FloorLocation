93f6805bb188c2e3eaadd0fbd440945f181f2fc2
Carl Olsen <carl-olsen@mchsi.com>
2024-01-07 17:30:45 -0600

    Create LocationsController.cs

---
+++ FloorLocation/Controllers/LocationsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FloorLocation.Models;
+
+namespace FloorLocation.Controllers
+{
+    [Route("api/locations")]
+    [ApiController]
+    public class LocationsController : Controller
+    {
+        [HttpPost]
+        public IActionResult Locations()
+        {
+            Context context = new();
+            List<Location> list = context.GetLocations();
+            return Ok(list);
+        }
+    }
+}
+
